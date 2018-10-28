b1ab47b6c15de8cae08087b35ad26139e1635ef4
Matthew Matz <19737272+MatzElectronics@users.noreply.github.com>
2018-10-27 23:50:39 -0700

    Update test_ir_slow_roaming.py

--- library/test_ir_slow_roaming.py
+++ library/test_ir_slow_roaming.py
@@ -2,30 +2,30 @@
 # IR Detection - Slow Roaming #
 ###############################
 
-bot_pin(22).frequency_out(500, 1000)
+bot(22).frequency_out(500, 1000)
  
 while True:
 
-    # Detect is the IR LED, pin is receiver
-    irR = bot_pin(3).ir_detect(1, 37500)
-    irL = bot_pin(9).ir_detect(11, 35700)
+    # Pin is receiver, detect is the IR LED 
+    irR = bot(3).ir_detect(1, 37500)
+    irL = bot(9).ir_detect(11, 35700)
     
-    bot_pin(21).digital_write(irR)
-    bot_pin(20).digital_write(irL)
+    bot(21).digital_write(irR)
+    bot(20).digital_write(irL)
     
     if irL == 1 and irR == 1:
-        bot_pin(18).servo_speed(32)
-        bot_pin(19).servo_speed(-37)
+        bot(18).servo_speed(32)
+        bot(19).servo_speed(-37)
         display.show(Image.HAPPY)
     elif irL == 0 and irR == 0:
-        bot_pin(18).servo_speed(-32)
-        bot_pin(19).servo_speed(32)
+        bot(18).servo_speed(-32)
+        bot(19).servo_speed(32)
         display.show(Image.SAD)
     elif irL == 0:
-        bot_pin(18).servo_speed(32)
-        bot_pin(19).servo_speed(32)
+        bot(18).servo_speed(32)
+        bot(19).servo_speed(32)
         display.show(Image.SAD)
     elif irR == 0:
-        bot_pin(18).servo_speed(-32)
-        bot_pin(19).servo_speed(-32)
+        bot(18).servo_speed(-32)
+        bot(19).servo_speed(-32)
         display.show(Image.SAD)
