32cb124a0820f235ed7ba768a3e89de66cd65eaa
Tommy Gober <tommy@neodux.com>
2018-10-29 14:46:51 -0500

    IR controls to drive robot. FWD, BACK, LEFT, RIGHT
    Microbit display shows direction of travel.
    Buttons 2, 4, 6, 8 for F, B, L, R
    Button 5 will stop.

---
+++ examples/IR_Control_Robot.py
@@ -0,0 +1,28 @@
+##################################
+# IR Control Robot               #
+# IR detector connected to pin 0 #
+##################################
+
+while True:
+    num = bot(0).tv_remote()
+    
+    if num == 2:
+        bot(18).servo_speed(75)
+        bot(19).servo_speed(-75)
+        display.show(Image.ARROW_S)
+    elif num == 4:
+        bot(18).servo_speed(-75)
+        bot(19).servo_speed(-75)
+        display.show(Image.ARROW_E)
+    elif num == 5:
+        bot(18).servo_speed(0)
+        bot(19).servo_speed(0)
+        display.show(Image.HAPPY)
+    elif num == 6:
+        bot(18).servo_speed(75)
+        bot(19).servo_speed(75)
+        display.show(Image.ARROW_W)
+    elif num == 8:
+        bot(18).servo_speed(-75)
+        bot(19).servo_speed(75)
+        display.show(Image.ARROW_N)
