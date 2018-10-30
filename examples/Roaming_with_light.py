56c746e5010fbb0287bd8409a0687cca162e3c9f
Matthew Matz <19737272+MatzElectronics@users.noreply.github.com>
2018-10-29 22:00:42 -0700

    Update Roaming_with_light.py

--- examples/Roaming_with_light.py
+++ examples/Roaming_with_light.py
@@ -1,38 +1,33 @@
-# WORK IN PROGRESS
-
 bot(22).frequency_out(500, 1000)
 
 while True:
 
-	# read the phototransistors
-	pL = bot(9).rc_time(1)
-	pR = bot(5).rc_time(1)
+    # read the phototransistors
+    pL = bot(9).rc_time(1)
+    pR = bot(5).rc_time(1)
 
-	# show the phototransistors sensors on the display
-	# creates a log-scaled bar graph
-	gL = trunc(log(pL, 10)) + 1
-	gR = trunc(log(pR, 10)) + 1
-	for y in range(0,4):
-		if y <= gL:
-			display.set_pixel(4, y, 9)
-		else:
-			display.set_pixel(4, y, 0)
-		if y <= gR:
-			display.set_pixel(0, y, 9)
-		else:
-			display.set_pixel(0, y, 0)
+    # make a "log-scale" graph by
+    # turning the sensor readings into 
+    # strings and counting the digits
+    gL = 4 - len(str(pL * 4))
+    gR = 4 - len(str(pR * 4))
+    
+    for y in range(0,4):
+        if y <= gL:
+            display.set_pixel(4, y, 9)
+        else:
+            display.set_pixel(4, y, 0)
+        if y <= gR:
+            display.set_pixel(0, y, 9)
+        else:
+            display.set_pixel(0, y, 0)
 
-	# calculate the normalized differential of the measurements
-	nDiff = trunc((200 * pR) / (pR + pL) - 100)
+    # set the wheel speed variables according
+    # to the normalized differential
+    nDiff = round((150 * pR) / (pR + pL + 1) - 75)
 
-	# set the wheel speed variables according
-	# to the normalized differential
-	wR = 64
-	wL = 64
-	if nDiff > 0:
-		wL = 64 - nDiff
-	else:
-		wR = 64 + nDiff
-
-	bot(16).servo_speed(wL)
-	bot(17).servo_speed(-wR)
+    wL = 64 - nDiff
+    wR = -64 - nDiff
+    
+    bot(18).servo_speed(wL)
+    bot(19).servo_speed(wR)
