a8d9c5675623dcf43f2ef4acec7f4a3ac11353a5
Tommy Gober <tommy@neodux.com>
2018-10-30 21:35:44 -0500

    Create Whisker_Roaming.py

---
+++ examples/Whisker_Roaming.py
@@ -0,0 +1,37 @@
+##################################
+# Whisker Roaming                #
+##################################
+
+while True:
+    left = bot(6).digital_read()
+    right = bot(8).digital_read()
+    
+    if left == 1 and right == 1: #Go forward
+        bot(18).servo_speed(75)
+        bot(19).servo_speed(-75)
+        display.show(Image.ARROW_S)
+    elif left == 1 and right == 0: #Obstacle on right
+        bot(18).servo_speed(-75)    #back up for 1s, turn left
+        bot(19).servo_speed(75)
+        display.show(Image.ARROW_N)
+        sleep(1000)
+        bot(18).servo_speed(-75)
+        bot(19).servo_speed(-75)
+        display.show(Image.ARROW_E)
+    elif left == 0 and right ==1:  #Obstacle on left
+        bot(18).servo_speed(-75)    #backup for 1s, turn right
+        bot(19).servo_speed(75)
+        display.show(Image.ARROW_N)
+        sleep(1000)
+        bot(18).servo_speed(75)
+        bot(19).servo_speed(75)
+        display.show(Image.ARROW_W)
+    elif left == 0 and right == 0: #Obstacle on left + right
+        bot(18).servo_speed(-75)    #backup for 1s, turn
+        bot(19).servo_speed(75)
+        display.show(Image.ARROW_N)
+        sleep(1000)
+        bot(18).servo_speed(75)
+        bot(19).servo_speed(75)
+        display.show(Image.ARROW_W)
+        sleep(750)
