e268c9839240d7db087b2bdf0d43e569e62aab4d
Tommy Gober <tommy@neodux.com>
2018-10-30 21:01:49 -0500

    Ping sensor works as expected...

---
+++ examples/Ping_Test.py
@@ -0,0 +1,8 @@
+##################################
+# Ping Test                      #
+##################################
+
+while True:
+    dist = bot(8).ping_distance(u="in")
+    display.scroll(str(dist), 125)
+    
