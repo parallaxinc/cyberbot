dd23f223de44b25010a0cc80b715daccf66141d0
Tommy Gober <tommy@neodux.com>
2018-10-30 21:40:00 -0500

    Update Whisker_Detection.py

--- examples/Whisker_Detection.py
+++ examples/Whisker_Detection.py
@@ -7,10 +7,10 @@ while True:
     rightWhisker = bot(8).digital_read()
     
     if leftWhisker == 0:
-        display.set_pixel(0, 2, 9)
+        display.set_pixel(0, 2, 9) #left LED lights if left whisker is pressed
     else:
         display.set_pixel(0, 2, 0)
     if rightWhisker == 0:
-        display.set_pixel(4, 2, 9)
+        display.set_pixel(4, 2, 9) #right LED lights if right whisker is pressed
     else:
         display.set_pixel(4, 2, 0)
