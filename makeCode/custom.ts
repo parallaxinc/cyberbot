2978742b47ccce22f46ef0ae0d78c89c3506cf56
Matthew Matz <19737272+MatzElectronics@users.noreply.github.com>
2018-12-12 00:29:57 -0800

    Create custom.ts
    Initial commit for blocks in Microsoft MakeCode for the Micro:bit

---
+++ makeCode/custom.ts
@@ -0,0 +1,152 @@
+
+/**
+ * Use this file to define custom functions and blocks.
+ * Read more at https://makecode.microbit.org/blocks/custom
+ */
+
+
+// Initialize routine - may need to move into a block that the user calls "on startup"
+while (pins.digitalReadPin(DigitalPin.P8) == 0);
+basic.pause(1)
+pins.i2cWriteNumber(83, 0, NumberFormat.UInt8LE, true)
+pins.i2cWriteNumber(83, 99, NumberFormat.UInt8LE, false)
+basic.pause(10)
+pins.digitalWritePin(DigitalPin.P8, 1)
+basic.pause(10)
+
+
+enum MyEnum {
+    //% block="one"
+    One,
+    //% block="two"
+    Two
+}
+
+enum botPin {
+    //% block="P0"
+    P0,
+    //% block="P1"
+    P1,
+    //% block="P2"
+    P2,
+    //% block="P3"
+    P3,
+    //% block="P4"
+    P4,
+    //% block="P5"
+    P5,
+    //% block="P6"
+    P6,
+    //% block="P7"
+    P7,
+    //% block="P8"
+    P8,
+    //% block="P9"
+    P9,
+    //% block="P10"
+    P10,
+    //% block="P11"
+    P11,
+    //% block="P12"
+    P12,
+    //% block="P13"
+    P13,
+    //% block="P14"
+    P14,
+    //% block="P15"
+    P15,
+    //% block="P16"
+    P16,
+    //% block="P17"
+    P17,
+    //% block="P18"
+    P18,
+    //% block="P19"
+    P19,
+    //% block="P20"
+    P20,
+    //% block="P21"
+    P21,
+    //% block="P22"
+    P22
+}
+
+/**
+ * Cyberbot blocks
+ */
+//% weight=100 color=#000000 icon="\uf1b9"
+namespace cyberbot {
+    export function sendCommand(pin1: number, cmd: number, pin2: number, states: number, arg1: number, arg2: number) {
+        let outStop0 = true
+        let outStop1 = true
+        if (!arg1 && !arg2) { outStop0 = false }
+        if (arg1 && !arg2) { outStop1 = false }
+        pins.i2cWriteNumber(83, 1, NumberFormat.UInt8LE, true)
+        pins.i2cWriteNumber(83, pin1, NumberFormat.UInt8LE, true)
+        pins.i2cWriteNumber(83, pin2 || 0, NumberFormat.UInt8LE, true)
+        pins.i2cWriteNumber(83, states || 0, NumberFormat.UInt8LE, outStop0)
+        if (arg1) {
+            pins.i2cWriteNumber(83, arg1, NumberFormat.UInt32LE, outStop1)
+        }
+        if (arg2) {
+            pins.i2cWriteNumber(83, arg2, NumberFormat.UInt32LE, false)
+        }
+        pins.i2cWriteNumber(83, 0, NumberFormat.UInt8LE, true)
+        pins.i2cWriteNumber(83, cmd, NumberFormat.UInt8LE, false)
+        cmd = 1
+        while (cmd != 0) {
+            pins.i2cWriteNumber(83, 0, NumberFormat.UInt8LE, true)
+            cmd = pins.i2cReadNumber(83, NumberFormat.UInt8LE, false)
+        }
+    }
+
+    /**
+     * Set a pin to output 0 (low), 1 (high), or 2 (toggle)
+     * @param p pin
+     * @param s what state to set the pin to
+     */
+    //% block
+    export function digitalWrite(p: botPin, s: number): void {
+        if (s < 0 || s > 1) { s = 4 }
+        if (s == 0) { s = 2 }
+        sendCommand(p, s, 0, 0, null, null)
+    }
+
+    /**
+     * Set a pin to output an analog voltage (using PWM)
+     * @param p pin
+     * @param value to set PWM duty cycle to (from 0 - 0% to 1023 - 100%)
+     */
+    //% block
+    export function analogWrite(p: botPin, value: number): void {
+        if (value < 0) value = 0;
+        if (value > 1023) value = 1023;
+        sendCommand(p, 32, 0, 0, value, null)
+    }
+
+    /**
+     * Read the digital state of a pin
+     * @param p pin
+     * @returns 0 (low) or 1 (high)
+     */
+    //% block
+    export function digitalRead(p: botPin): number {
+        sendCommand(p, 3, 0, 0, null, null)
+        pins.i2cWriteNumber(83, 24, NumberFormat.UInt8LE, true)
+        return pins.i2cReadNumber(83, NumberFormat.UInt8LE, false)
+    }
+    
+    /**
+     * Read the digital states of a group of pins
+     * @param p end pin
+     * @param p2 start pin
+     * @returns binary value representing states of each pin
+     */
+    //% block="read digital states from high pin $p|to low pin $p2"
+    //% inlineInputMode=inline
+        export function readDigitalStates(p: botPin, p2: botPin): number {
+        //sendCommand(p, 3, 0, 0, null, null)
+        //pins.i2cWriteNumber(83, 24, NumberFormat.UInt8LE, true)
+        return 0 //pins.i2cReadNumber(83, NumberFormat.UInt8LE, false)
+    }
+}
