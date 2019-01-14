fb7d998353d63ec17a7f4dacff56d8be87bc42cb
Andy Lindsay <alindsay@parallax.com>
2019-01-14 11:58:57 -0800

    File and folder maintenance

--- firmware/Dev/Tests_Blink_Rate.py
+++
@@ -1,9 +0,0 @@
-# Test_Blink_Rate.py
-# Use oscilloscope on P7 to measure cycle time
-
-from microbit import *
-from parallax import bot
-
-while True:
-    bot(7).digital_write(1)
-    bot(7).digital_write(0)
